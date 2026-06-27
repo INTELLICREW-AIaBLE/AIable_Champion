fb7bb8241bfc9589614f7a8e496befe16049e4fe
horizon <89583473+syntune@users.noreply.github.com>
2026-06-27 21:16:14 +0700

    Added AppShell contents

---
+++ AIaBLE-Project/frontend/src/components/AppShell.tsx
@@ -0,0 +1,7 @@
+import Link from "next/link";
+
+<Link href="/optimizer">Optimizer</Link>
+<Link href="/task-matcher">Task Matcher</Link>
+<Link href="/recipes">Recipes</Link>
+<Link href="/validator">Validator</Link>
+<Link href="/sandbox">Sandbox</Link>
